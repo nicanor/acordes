3f7d3c35e39a32182fc5539eb2e234326ad161c7
Nicanor Perera <nicanorperera@gmail.com>
2019-04-16 17:36:01 -0300

    Using grep for filter

--- lib/acordes/hub/fuzzy_filter.ex
+++ lib/acordes/hub/fuzzy_filter.ex
@@ -4,22 +4,48 @@ defmodule Acordes.Hub.FuzzyFilter do
 
   @file_path Path.join(File.cwd!(), "priv/repo/search_terms.txt")
 
+  def get4(term) do
+    new_term = "%#{term}%"
+
+    artists =
+      Repo.all(
+        from a in Acordes.Hub.Artist, where: ilike(a.name, ^new_term), select: [a.name, a.slug]
+      )
+
+    tabs =
+      Repo.all(
+        from t in Acordes.Hub.Tab, where: ilike(t.title, ^new_term), select: [t.title, t.slug]
+      )
+
+    artists ++ tabs
+  end
+
+  def get(term) do
+    "cat #{@file_path} | grep -i \"#{term}\""
+    |> String.to_charlist()
+    |> :os.cmd()
+    |> List.to_string()
+    |> String.split("\n", trim: true)
+    |> Stream.take(200)
+    |> Enum.map(&split/1)
+  end
+
   def get2(term) do
     # "to_tsvector('spanish', name) @@ plainto_tsquery('spanish', :q)"
 
     Repo.all(
-      from a in Acordes.Hub.Artist,
+      from a in Acordes.Hub.Tab,
         where:
           fragment(
             "to_tsvector('spanish', ?) @@ plainto_tsquery('spanish', ?)",
-            a.name,
+            a.title,
             ^(term <> ":*")
           ),
-        select: a.name
+        select: [a.title, a.slug]
     )
   end
 
-  def get(term) do
+  def get3(term) do
     "cat #{@file_path} | fzf --filter=\"#{term}\""
     |> String.to_charlist()
     |> :os.cmd()
